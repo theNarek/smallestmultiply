c571d8887b191e8c49bee03045b8ebba63dd0c99
Narek <avanesyan12345@gmail.com>
2020-11-20 08:27:17 +0100

    Added the largest dommon divider files

---
+++ Largest common divider/script.js
@@ -0,0 +1,12 @@
+function solve() {
+    var firstNum = Number(document.getElementById('firstNum').value),
+        secondNum = Number(document.getElementById('secondNum').value),
+        resultBox = document.getElementById('resultBox');
+
+    for (var i = 0; i <= firstNum && i <= secondNum; i++) {
+        if (firstNum % i == 0 && secondNum % i == 0) {
+            var lastNum = i;
+            resultBox.innerHTML = '<p style="color: green;">' + firstNum + ' և ' + secondNum + ' թվերի ամենամեծ ընդհանուր բաժանարարը ' + lastNum + '-ն է։ </p>';
+        }
+    }
+}
